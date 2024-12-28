8f48b1b8ffc4b3074212ac4cedc0053ba96baa80
Shubham <spasd009@gmail.com>
2024-12-28 10:15:27 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-900">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
